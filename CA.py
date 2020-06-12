6257fd0374915a609fb7f1c134a86ce9ed100533
arifb <arifbayirli@gmail.com>
2020-06-12 13:39:08 +0200

    Rule 1-2-3-4 added

--- CA.py
+++ CA.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 from rules import rule_1, rule_2, rule_3, rule_4
 
 def main():
-	space_dim = 100
-	time_dim = 130
+	space_dim = 45
+	time_dim = 45
 	image = np.zeros((time_dim, space_dim + 1), dtype='int32')
 	image[0, (space_dim + 1)//2] = 1
 
 	for t in range(1,time_dim):
-		image[t] = rule_3(image[t-1])
+		image[t] = rule_4(image[t-1])
 
-	plt.figure(figsize = (8,6))
+	#print(image)
+	#plt.figure(figsize = (8,6))
 	plt.imshow(image, cmap='Greys', interpolation = "nearest")
+	plt.savefig("Rule_4.png")
 	plt.show()
 
 
